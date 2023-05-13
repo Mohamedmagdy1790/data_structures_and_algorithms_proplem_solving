3a79ea4ab1ac62d1fe8ef989a5744e0bdf7dbd36
Mohamedmagdy1790 <mmagdy3396@gmail.com>
2023-05-14 01:23:19 +0300

    created

---
+++ data_structure/vector_implmentaion/src/vectorpac/Main.java
@@ -0,0 +1,29 @@
+package vectorpac;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+      vector p1 =new vector(3);
+
+      p1.pushback(10);
+      p1.pushback(40);
+      p1.pushback(30);
+      p1.pushback(70);
+
+      p1.print();
+
+      System.out.println("");
+
+      p1.Rotate_right();
+      p1.print();
+
+      System.out.println("");
+
+      p1.Rotate_left();
+      p1.print();
+
+
+    }
+
+}
